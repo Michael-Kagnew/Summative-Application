1c75a59678fd5dc3326b194dfe9d10862df5aae1
Mike Kagnew <44201432+FinesseMaster@users.noreply.github.com>
2019-01-14 20:06:04 -0500

    EXTREME ROUGH DRAFT. Work in progress.
    Need to figure out how to create cells, import month, etc.,. May revamp this.

---
+++ Code/Calendar.java
@@ -0,0 +1,47 @@
+package calendar;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+//import com.mindfusion.scheduling.Calendar;
+import com.mindfusion.common.*;
+import com.mindfusion.common.Rectangle;
+import com.mindfusion.drawing.*;
+import com.mindfusion.drawing.awt.AwtImage;
+import com.mindfusion.scheduling.*;
+import com.mindfusion.scheduling.awt.*;
+import com.mindfusion.scheduling.model.*;
+import java.awt.BorderLayout;
+
+
+
+
+public class Calendar extends JFrame {
+   protected Calendar()
+    {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setTitle("Calendar");
+    }
+    private static final long serialVersionUID = 1L;
+
+       public static void main(String[] args) {
+        
+        
+        Calendar calendar = new Calendar();
+        
+        //Intializes the whole app, letting it be seen
+        SwingUtilities.invokeLater(new Runnable() {
+             public void run() {
+                     new Calendar().setVisible(true);
+               
+         };
+    });
+    }
+}
+ 
+
+    
+
+    
+    
+
