d961954cb646224390adf66f645c4cfaabac3d6a
Mike Kagnew <44201432+FinesseMaster@users.noreply.github.com>
2019-01-16 09:45:55 -0500

    Functions that will control how the calendar works

---
+++ Code/CalendarControl.java
@@ -0,0 +1,51 @@
+
+package calendarprogram2;
+
+import java.util.*;
+import com.mindfusion.scheduling;
+import com.mindfusion.scheduling.Calendar;
+public class CalendarControl extends CalendarProgram2{
+  
+    
+    public void setCurrentView() {
+    CalendarView Timetable;
+            }
+    
+    
+    public static void refreshCalendar(int month, int year){
+		//Variables
+		String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		int nod, som; //Number Of Days, Start Of Month
+			
+		//Allow/disallow buttons
+		btnPrev.setEnabled(true);
+		btnNext.setEnabled(true);
+		if (month == 0 && year <= realYear-10){btnPrev.setEnabled(false);} //Too early
+		if (month == 11 && year >= realYear+100){btnNext.setEnabled(false);} //Too late
+		lblMonth.setText(months[month]); //Refresh the month label (at the top)
+		lblMonth.setBounds(160-lblMonth.getPreferredSize().width/2, 25, 180, 25); //Re-align label with calendar
+		cmbYear.setSelectedItem(String.valueOf(year)); //Select the correct year in the combo box
+		
+		//Clear table
+		for (int i=0; i<6; i++){
+			for (int j=0; j<7; j++){
+				mtblCalendar.setValueAt(null, i, j);
+			}
+		}
+		
+		//Get first day of month and number of days
+		GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+		nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+		som = cal.get(GregorianCalendar.DAY_OF_WEEK);
+		
+		//Draw calendar
+		for (int i=1; i<=nod; i++){
+			int row = new Integer((i+som-2)/7);
+			int column  =  (i+som-2)%7;
+			mtblCalendar.setValueAt(i, row, column);
+		}
+
+		//Apply renderers
+		tblCalendar.setDefaultRenderer(tblCalendar.getColumnClass(0), new tblCalendarRenderer());
+	}
+}
